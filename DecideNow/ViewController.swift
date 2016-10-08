fa5325d69441e7169f5c1c7ac615a5c43f8de0be
ilovemochida <mochida@pictors.co.jp>
2016-10-08 15:34:37 +0900

    first

---
+++ DecideNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DecideNow
+//
+//  Created by 持田章弘 on 2016/10/08.
+//  Copyright © 2016年 500hack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
