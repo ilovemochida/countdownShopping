2d925775c752bf654ebe2f0b8e97e09c5f8d0ba4
ilovemochida <mochida@pictors.co.jp>
2016-10-09 12:47:44 +0900

    make textfield class of inputting the security code

--- DecideNow/SecurityCodeForm.swift
+++ DecideNow/SecurityCodeForm.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2016年 500hack. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+
+extension UITextField{
+    class func makeSecurityCodeForm(y: CGFloat) -> UITextField{
+        let form = UITextField()
+        form.frame = CGRect(x: 30, y: y, width: Const.SCREEN_WIDTH - 60, height: 30)
+        form.placeholder = "security code"
+        form.keyboardType = .numberPad
+        form.backgroundColor = UIColor.white
+        form.borderStyle = UITextBorderStyle.roundedRect
+        return form
+    }
+}
